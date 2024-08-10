e37b0d6761f747b7fa8eb2ae2b5b00abebf7f839
emersonpessoa01 <eps011108@gmail.com>
2024-08-10 11:43:08 -0300

    fix: adjusted Image :rocket:

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import logo from "../image/Logo.png";
+import logo from "../../public/image/logo.png";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="flex flex-wrap items-center justify-between p-5">
+    <Card className="fixed left-0 top-0 z-50 w-full">
+      <CardContent className="flex flex-wrap items-center justify-between p-3">
         <Image alt="FSW-barber" src={logo} width={120} height={18} />
         <Button size="icon" variant="outline">
           <MenuIcon />
--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,54 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
-import Banner from "./image/Banner01.png";
+import Mobile from "../public/image/banner-mobile.png";
+import { Card, CardContent } from "./_components/ui/card";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <div className="p-5">
+      <div className="px-4 pt-20">
         <h2 className="text-xl font-bold">Olá, Emerson</h2>
         <p>Quarta-feira, 07 de Agosto</p>
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-        <div className="mt-6 h-[150px]">
+        {/* IMAGEM */}
+        {/* <div className="relative mt-6 h-[400px]">
           <Image
             src={Banner}
             alt={"Agende nos melhores com FWS Barber"}
-            className="fill w-full object-cover"
+            fill
+            className=" w-full object-cover"
           />
+        </div> */}
+        <div className="relative mt-6 h-[450px] w-full">
+          <picture>
+            <source
+              srcSet="/image/banner-desktop.png"
+              media="(min-width: 768px)"
+            />
+            <Image
+              src={Mobile}
+              alt="Banner"
+              fill
+              className="rounded-xt h-full w-full object-cover"
+            />
+          </picture>
         </div>
+        {/* AGENDAMENTO */}
+        <Card className="">
+          <CardContent>
+            <h3 className="text-lg font-bold">Agendamento</h3>
+            <p>Selecione um horário para começar</p>
+            <Button className="w-full">Agendar</Button>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
