00d6809810f0ad2203d29f2862aed5ed7206dfd2
emersonpessoa01 <eps011108@gmail.com>
2024-08-11 14:39:25 -0300

    feat: add booking item :rocket:

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import Mobile from "../public/image/banner-mobile.png";
 import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return (
@@ -29,7 +31,7 @@ const Home = () => {
             className=" w-full object-cover"
           />
         </div> */}
-        <div className="relative mt-6 h-[450px] w-full">
+        <div className="relative mt-6 h-[400px] w-full">
           <picture>
             <source
               srcSet="/image/banner-desktop.png"
@@ -44,11 +46,25 @@ const Home = () => {
           </picture>
         </div>
         {/* AGENDAMENTO */}
-        <Card className="">
-          <CardContent>
-            <h3 className="text-lg font-bold">Agendamento</h3>
-            <p>Selecione um horário para começar</p>
-            <Button className="w-full">Agendar</Button>
+        <Card className="mt-6">
+          <CardContent className="flex flex-wrap justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* DIREITA  */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
           </CardContent>
         </Card>
       </div>
